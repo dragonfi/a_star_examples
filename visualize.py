1b1a34d682205ece4a23626c936e84c87b005fd1
David Gabor Bodor <david.gabor.bodor@gmail.com>
2019-07-11 13:12:02 +0200

    visualize connections

--- visualize.py
+++ visualize.py
@@ -10,8 +10,7 @@ label = pyglet.text.Label('Hello, world',
                           x=window.width//2, y=window.height//2,
                           anchor_x='center', anchor_y='center')
 
-def point_to_opengl(point):
-    r = 5
+def point_to_opengl(point, r = 2):
     points = [
         point.x - r, point.y - r,
         point.x - r, point.y + r,
@@ -21,7 +20,7 @@ def point_to_opengl(point):
     indexes = [0, 1, 2, 1, 2, 3]
     return (indexes, points)
 
-def draw_points(points):
+def draw_nodes(points):
     vertices = []
     index = []
     start_index = 0
@@ -36,10 +35,31 @@ def draw_points(points):
         index, ('v2f', vertices))
 
 
+def draw_edges(points, graph):
+    vertices = []
+    for point in points:
+        vertices += [point.x, point.y]
+
+    index = []
+    for node, edges in graph.items():
+        for edge in edges:
+            index += [node, edge.node]
+
+    print(len(vertices), len(index))
+
+    pyglet.graphics.draw_indexed(
+        len(vertices) // 2, pyglet.gl.GL_LINES,
+        index, ('v2f', vertices))
+
+from path_search.points import random_euclidean_points, connect_points
+
+POINTS = random_euclidean_points(1000, Vec2(50, 50), Vec2(590, 430))
+GRAPH = connect_points(POINTS, 20)
+
 @window.event
 def on_draw():
     window.clear()
-    label.draw()
+    #label.draw()
 
     # pyglet.graphics.draw(
     #     2, pyglet.gl.GL_POINTS,
@@ -56,11 +76,12 @@ def on_draw():
     #              150, 100,
     #              150, 150,
     #              100, 150))
-
-    draw_points([Vec2(10, 10), Vec2(20, 20), Vec2(30, 30)])
-
+    #
     # pyglet.graphics.draw_indexed(
     #     3, pyglet.gl.GL_TRIANGLES,
     #     [0, 1, 2], ('v2f', [10, 10, 20, 20, 10, 20]))
+
+    draw_nodes(POINTS)
+    draw_edges(POINTS, GRAPH)
 
 pyglet.app.run()
