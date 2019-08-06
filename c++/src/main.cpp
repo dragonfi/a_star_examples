60e19e965a25c321a29183d4dd2cb545a39eb825
Dávid Gábor Bodor <david.gabor.bodr@gmail.com>
2019-08-06 09:10:10 +0200

    feat(c++): add basic shaders

--- c++/src/main.cpp
+++ c++/src/main.cpp
@@ -11,6 +11,140 @@
 #include "pathing/graph.hpp"
 #include "graphics/glWindow.hpp"
 
+
+#include<fstream>
+#include<sstream>
+#include<string>
+namespace graphics {
+    class ShaderCompilationError: public std::exception {
+    public:
+        ShaderCompilationError(GLuint shaderId) {
+            m_what = "Shader compilation failed: ";
+
+            GLsizei maxLength;
+            glGetShaderiv(shaderId, GL_INFO_LOG_LENGTH, &maxLength);
+
+            char* infoLog = new char[maxLength];
+            GLsizei length;
+            glGetShaderInfoLog(shaderId, maxLength, &length, infoLog);
+            m_what += std::string(infoLog, length);
+            delete[] infoLog;
+        }
+        const char * what() const throw () {
+            return m_what.c_str();
+        }
+    private:
+        std::string m_what;
+    };
+
+    class ShaderLinkingError: public std::exception {
+    public:
+        ShaderLinkingError(GLuint shaderId) {
+            m_what = "Shader linking failed: ";
+
+            GLsizei maxLength;
+            glGetProgramiv(shaderId, GL_INFO_LOG_LENGTH, &maxLength);
+
+            char* infoLog = new char[maxLength];
+            GLsizei length;
+            glGetProgramInfoLog(shaderId, maxLength, &length, infoLog);
+            m_what += std::string(infoLog, length);
+            delete[] infoLog;
+        }
+        const char * what() const throw () {
+            return m_what.c_str();
+        }
+    private:
+        std::string m_what;
+    };
+
+    class Shader {
+    public:
+        Shader(const std::string& src, GLuint shader_type) {
+            shader = glCreateShader(shader_type);
+            const char * src_ = src.c_str();
+            int size = src.size();
+            glShaderSource(shader, 1, &src_, &size);
+            glCompileShader(shader);
+
+            int isCompiled = false;
+            glGetShaderiv(shader, GL_COMPILE_STATUS, &isCompiled);
+
+            if(isCompiled == false) {
+                throw ShaderCompilationError(shader);
+            }
+        }
+
+        GLuint id() {
+            return shader;
+        }
+        ~Shader() {
+            glDeleteShader(shader);
+        }
+
+    private:
+        GLuint shader = 0;
+    };
+
+    class ShaderProgram {
+    public:
+        ShaderProgram(const std::string& vert, const std::string& frag)
+            : vertexShader(vert, GL_VERTEX_SHADER)
+            , fragmentShader(frag, GL_FRAGMENT_SHADER)
+        {
+            shaderProgram = glCreateProgram();
+
+            glBindAttribLocation(shaderProgram, 0, "in_position");
+            glBindAttribLocation(shaderProgram, 1, "in_color");
+
+            glAttachShader(shaderProgram, vertexShader.id());
+            glAttachShader(shaderProgram, fragmentShader.id());
+
+            glLinkProgram(shaderProgram);
+
+            int isLinked = false;
+            glGetProgramiv(shaderProgram, GL_LINK_STATUS, &isLinked);
+
+            if(isLinked == false) {
+                throw ShaderLinkingError(shaderProgram);
+            }
+        }
+
+        void use() {
+            glUseProgram(shaderProgram);
+        }
+
+        ~ShaderProgram() {
+            glUseProgram(0);
+            glDetachShader(shaderProgram, vertexShader.id());
+            glDetachShader(shaderProgram, fragmentShader.id());
+        }
+    private:
+        GLuint shaderProgram = 0;
+        Shader vertexShader;
+        Shader fragmentShader;
+    };
+
+    class VAO {
+
+    };
+}
+
+std::string load_file(std::string file_name) {
+    std::ifstream f(file_name);
+    std::stringstream stream;
+    stream << f.rdbuf();
+
+    return stream.str();
+}
+
+void render_square(graphics::Renderer& renderer) {
+    graphics::ShaderProgram shader = graphics::ShaderProgram(
+        load_file("shaders/basic.vert"), load_file("shaders/basic.frag")
+    );
+    shader.use();
+}
+
 bool handle_key_down(const SDL_Event& event, graphics::Renderer& renderer) {
     const graphics::Color red(1.0, 0.0, 0.0);
     const graphics::Color green(0.0, 1.0, 0.0);
@@ -65,6 +199,8 @@ int main() {
     graphics::Color black(0.0, 0.0, 0.0);
     renderer.clear(black);
     renderer.swap();
+
+    render_square(renderer);
 
     main_loop(renderer);
 
---
+++ c++/src/shaders/basic.frag
@@ -0,0 +1,9 @@
+#version 120
+
+precision highp float;
+
+in vec4 ex_Color;
+
+void main(void) {
+    gl_FragColor = vec4(ex_Color);
+}
